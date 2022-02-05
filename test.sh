de02e24650b6f15a6b7310afc8a10ded9c96a0d0
Eugene <eugene.fedor@gmail.com>
2022-02-05 11:41:44 -0800

    alerts

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
 az deployment group create \
   --resource-group iot-e2e-rg1 \
-  --template-file dashboard.json \
+  --template-file MonitoringInstruments/alerts.json \
   --parameters iotHubName=iothub-a766ea18
