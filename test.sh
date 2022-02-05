e601c6156c12c278db2e4b1981670ad5db7f3be6
Eugene <eugene.fedor@gmail.com>
2022-02-04 19:02:07 -0800

    dashboard

--- test.sh
+++ test.sh
@@ -1,3 +1,4 @@
 az deployment group create \
-  --resource-group iot-e2e-rg \
-  --template-file dashboard.json
+  --resource-group iot-e2e-rg1 \
+  --template-file dashboard.json \
+  --parameters iotHubName=iothub-a766ea18
