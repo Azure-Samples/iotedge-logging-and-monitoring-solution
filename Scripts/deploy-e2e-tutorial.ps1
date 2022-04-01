5c8cce197aa1e98d7bbc68201c368e0884bdbb87
Eugene Fedorenko <eugene.fedor@gmail.com>
2022-04-01 00:29:32 +0000

    fix

--- Scripts/deploy-e2e-tutorial.ps1
+++ Scripts/deploy-e2e-tutorial.ps1
@@ -6,16 +6,18 @@
 
 Write-Host $env_hash
 Write-Host $output_parameters.environmentHashId.value
+Write-Host $output_parameters.iotHubName.value
+Write-Host output_parameters.edgeVmName.value
 
 &$PSScriptRoot/add_devices.sh $output_parameters.environmentHashId.value 10 $output_parameters.iotHubResourceGroup.value $output_parameters.iotHubName.value
 
 az iot hub module-twin update -n $output_parameters.iotHubName.value `
-                              -d $output_parametersoutput_parameters.edgeVmName.value `
+                              -d $output_parameters.edgeVmName.value `
                               -m "FilterModule" `
                               --desired '{\"minTemperatureThreshold\":30, \"maxTemperatureThreshold\":70}'
 
 az iot hub invoke-module-method --method-name 'RestartModule' `
-                              -n $output_parameters.iotHubName.value `c
+                              -n $output_parameters.iotHubName.value `
                               -d $output_parameters.edgeVmName.value `
                               -m '$edgeAgent' `
                               --method-payload `
