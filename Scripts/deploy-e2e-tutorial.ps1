7b9806cfd5db811911df9289cb98247a898dd001
Eugene Fedorenko <eugene.fedor@gmail.com>
2022-04-01 00:35:21 +0000

    e2e script

--- Scripts/deploy-e2e-tutorial.ps1
+++ Scripts/deploy-e2e-tutorial.ps1
@@ -4,11 +4,6 @@
 
 &$PSScriptRoot/deploy.ps1 "e2e"
 
-Write-Host $env_hash
-Write-Host $output_parameters.environmentHashId.value
-Write-Host $output_parameters.iotHubName.value
-Write-Host output_parameters.edgeVmName.value
-
 &$PSScriptRoot/add_devices.sh $output_parameters.environmentHashId.value 10 $output_parameters.iotHubResourceGroup.value $output_parameters.iotHubName.value
 
 az iot hub module-twin update -n $output_parameters.iotHubName.value `
